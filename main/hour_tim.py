f9f37a55f38b9a3dc9878bfb515d70a6dd45dd56
wrecks <rhaber@outlook.com>
2020-03-24 15:14:46 -0700

    Update hour_tim.py

--- main/hour_tim.py
+++ main/hour_tim.py
@@ -9,12 +9,12 @@ def time_corr():
     curr_min=RTC().datetime()[5] #current minutes
     curr_sec=RTC().datetime()[6] #current seconds
     i=0
-    min_diff=59-curr_min
-    sec_diff=59-curr_sec
+    min_diff=60-curr_min
+    sec_diff=60-curr_sec
     if min_diff==0:
-        slep_time=3600000-(1000*sec_diff)
+        slep_time=3660000-(1000*sec_diff)
     else:
-        slep_time=(min_diff*60000)-(1000*sec_diff)
+        slep_time=(min_diff*60000)+(60000)-(1000*sec_diff)
 
 
     
