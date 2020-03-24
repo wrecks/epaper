8a696c3b008bea9c399cc3bdc96b6c22298fc087
wrecks <rhaber@outlook.com>
2020-03-23 20:04:37 -0700

    Update volt.py

--- main/volt.py
+++ main/volt.py
@@ -5,9 +5,7 @@ import urequests
 from time import sleep
 import ads1x15
 
-i2c = machine.I2C(-1,scl=machine.Pin(35), sda=machine.Pin(33))
-i2c.init(scl=machine.Pin(12), sda=machine.Pin(33),freq=400000)
-
+i2c = machine.I2C(-1,scl=machine.Pin(32), sda=machine.Pin(27))
 ag_address=72 #analog address
 gain=0 #analog read gain (0-6.144v)
 adc_1115 = ads1x15.ADS1115(i2c, ag_address, gain) #voltage analog
